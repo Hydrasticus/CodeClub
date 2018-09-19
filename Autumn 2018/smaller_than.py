812bffa4080341e05b679f0feef0b10e5c510287
Alo Pullmann <alpull@ttu.ee>
2018-09-19 19:38:27 +0300

    First steps with Python (3/5).

---
+++ Autumn 2018/smaller_than.py
@@ -0,0 +1,7 @@
+def smaller_than(number_1, number_2):
+    if number_1 < number_2:
+        return number_1
+    elif number_2 < number_1:
+        return number_2
+    elif number_1 == number_2:
+        return None
